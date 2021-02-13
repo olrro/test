3cbfd838c7e8570bac5a65708034bfd4dfca864d
Your Name <you@example.com>
2021-02-14 02:38:48 +0300

    Update hook.php

--- hook.php
+++ hook.php
@@ -6,24 +6,11 @@ require_once __DIR__ . '/config.php';
 try {
 
     $bot = new \TelegramBot\Api\Client( $config['token'] );
-    $chat_id = $message->getChat()->getId();
 
-    if ( $chat_id != $config['admin_id'] ) {
-
-      $bot->sendMessage( $chat_id, 'Вы не администратор!' . PHP_EOL . 'Ваш ID - ' . $chat_id );
-
-    }
-    else {
-
-      $bot->command( 'dump', function ( $message ) use ( $bot ) {
-          $bot->sendMessage( $chat_id, json_encode( $message ) );
-      });
-
-      $bot->command( 'setadcount', function ( $message ) use ( $bot ) {
-          $bot->sendMessage( $chat_id, json_encode( $message ) );
-      });
-
-    }
+    $bot->command( 'dump', function ( $message ) use ( $bot ) {
+      $bot->sendMessage( $message->getFrom()->getId(), $bot->getRawBody() );
+      $bot->sendMessage( $message->getFrom()->getId(), json_encode( $message ) );
+    });
 
     $bot->run();
 
