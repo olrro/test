73951575d71186e6006d9a92a1a153e81470d88f
Your Name <you@example.com>
2021-02-14 11:28:27 +0300

    1

--- hook.php
+++ hook.php
@@ -1,20 +1,26 @@
 <?php
 
-require_once __DIR__ . '/api/autoload.php';
+require_once __DIR__ . '/api/predis/autoload.php';
+require_once __DIR__ . '/api/telegram/autoload.php';
 require_once __DIR__ . '/config.php';
 
 try {
+
+    Predis\Autoloader::register();
+
+    $redis = new Predis\Client( getenv( 'REDIS_URL' ) );
+    $redis->set( 'dick', 'pick' );
 
     $bot = new \TelegramBot\Api\Client( $config['token'] );
 
     $bot->command( 'dump', function ( $message ) use ( $bot ) {
       $bot->sendMessage( $message->getFrom()->getId(), $bot->getRawBody() );
-      $bot->sendMessage( $message->getFrom()->getId(), json_encode( $message ) );
+      $bot->sendMessage( $message->getFrom()->getId(), $redis->get('dick') );
     });
 
     $bot->run();
 
-} catch (\TelegramBot\Api\Exception $e) {
+} catch (\Exception $e) {
 
     $e->getMessage();
 
