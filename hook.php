d30e2a430fd3c8bc34c761132da99f3863a05151
Your Name <you@example.com>
2021-02-14 11:33:26 +0300

    Update hook.php

--- hook.php
+++ hook.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 require_once __DIR__ . '/api/predis/autoload.php';
 require_once __DIR__ . '/api/telegram/autoload.php';
 require_once __DIR__ . '/config.php';
@@ -10,6 +14,8 @@ try {
 
     $redis = new Predis\Client( getenv( 'REDIS_URL' ) );
     $redis->set( 'dick', 'pick' );
+
+    echo $redis->get('dick');
 
     $bot = new \TelegramBot\Api\Client( $config['token'] );
 
